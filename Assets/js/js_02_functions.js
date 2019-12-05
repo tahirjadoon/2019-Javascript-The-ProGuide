587d40f2f8d646e7db9a2e0b96b0c865777afe99
Tahir Jadoon <tjadoon@gmail.com>
2019-12-04 21:28:20 -0500

    Additional function concepts

--- Assets/js/js_02_functions.js
+++ Assets/js/js_02_functions.js
@@ -89,3 +89,96 @@ const myPerson = loadPerson('John');
 console.log(myPerson.name);
 //Noteworthy: Extra parentheses are required around the object, since the curly braces would otherwise be interpreted as the function body delimiters (and hence a syntax error would be thrown here).
 
+//#9. REST operator, can pass any number of params
+const sumUp = (...numbers) => {
+    let sum = 0;
+    for (const num of numbers){
+      sum += num;
+    }
+    return sum;
+  };
+  console.log("REST operator #9: "+ sumUp(1,2,-4,5,9));
+  console.log("REST operator #9: "+ sumUp(4,3,6,7,-4,6,5,-2));
+
+  //REST operator with function keyword, use arguments keyword
+const subtractUp = function(...numbers) {
+    let result = 0;
+    for (const num of arguments){
+        result -= num;
+    }
+    return result;
+};
+console.log(subtractUp(1,2,-4,5,9));
+console.log(subtractUp(4,3,6,7,-4,6,5,-2));
+
+//#10. Writing a function inside a function
+const sumUp2 = (...numbers) => {
+    //validate numbers
+    const validateNumber = num => {
+        //if not a number then return 0
+        return isNaN(num) ? 0 : +num;
+    };
+
+    let sum = 0;
+    for (const num of numbers){
+        sum += validateNumber(num);
+    }
+    return sum;
+};
+console.log("Writing a function inside a function #10: "+sumUp2(1,2,-4,5,9, 'B'));
+console.log("Writing a function inside a function #10: "+sumUp2(4,3,6,7,-4,6,5,-2, 'A'));
+
+//#11. CallBack Function 
+//result of sum is printed by this function which is a callback function
+const myResultHandlerFunction = sum => {
+console.log("CallBack function #11: " + sum);
+};
+
+//resultHandler is the callback function
+const sumUp3 = (resultHandler, ...numbers) => {
+    //validate numbers
+    const validateNumber = num => {
+        //if not a number then return 0
+        return isNaN(num) ? 0 : +num;
+    };
+
+    let sum = 0;
+    for (const num of numbers){
+        sum += validateNumber(num);
+    }
+    //call the callback funcion
+    resultHandler(sum);
+};
+sumUp3(myResultHandlerFunction, 1,2,-4,5,9, 'B');
+sumUp3(myResultHandlerFunction, 4,3,6,7,-4,6,5,-2, 'A');
+
+//#12. BIND()
+//result of sum is printed by this function which is a callback function
+//since we are binding the message, it would be the first parameter
+const addSubtractShowMessage = (message, result) => {
+    console.log(message.toString()+ result);
+};
+
+//resultHandler is the callback function
+const addSubtract = (resultHandler, operation, ...numbers) => {
+    //validate numbers
+    const validateNumber = num => {
+        //if not a number then return 0
+        return isNaN(num) ? 0 : +num;
+    };
+
+    let sum = 0;
+    for (const num of numbers){
+        if (operation === 'ADD')
+        sum += validateNumber(num);
+        else
+        sum -= validateNumber(num);
+    }
+    //call the callback funcion, showing message via BIND()
+    resultHandler(sum);
+};
+//call the operation function with a CallaAck function and Bind the message to display
+//here the first argument would be 'this' keyword and second argument would be the message
+addSubtract(addSubtractShowMessage.bind(this, 'Result of ADD operation #12: '), 'ADD', 1,2,-4,5,9, 'B');
+addSubtract(addSubtractShowMessage.bind(this, 'Result of ADD operation #12: '), 'ADD', 6,2,-4,'C',7,9, 'B');
+addSubtract(addSubtractShowMessage.bind(this, 'Result of SUBTRACT operation #12: '), 'SUBTRACT', 4,3,6,7,-4,6,5,-2, 'A');
